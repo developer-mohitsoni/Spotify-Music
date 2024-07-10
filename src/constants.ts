e67c8529df38a9ee61380e69933adfe9a222f73f
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-10 20:39:50 +0530

    creating some constants object of MusicPlayer media used

---
+++ src/constants.ts
@@ -0,0 +1,69 @@
+
+import { Track } from "react-native-track-player";
+
+export const playListData: Track[] = [
+    {
+      id: 1,
+      title: 'AC Odyssey Myrrine OST',
+      artist: 'The Flight, Assassin Creed',
+      album: 'Assassin Creeed',
+      artwork:
+        'https://c.saavncdn.com/661/Assassin-s-Creed-Odyssey-Original-Game-Soundtrack--English-2018-20180924194240-500x500.jpg',
+      url: require('./assets/audio/ACOST.mp3'),
+    },
+    {
+      id: 2,
+      title: 'Endless Love',
+      artist: 'DVRST, OBLXKQ',
+      album: 'Phonk',
+      artwork:
+        'https://c.saavncdn.com/081/Endless-Love-English-2021-20210923015825-500x500.jpg',
+      url: require('./assets/audio/Endless-Love.mp3'),
+    },
+    {
+      id: 3,
+      title: 'One Chance',
+      artist: 'MoonDeity',
+      album: 'Phonk',
+      artwork:
+        'https://c.saavncdn.com/111/One-Chance-English-2023-20230104225953-500x500.jpg',
+      url: require('./assets/audio/Interworld-Moondenity.mp3'),
+    },
+    {
+      id: 4,
+      title: 'Motion',
+      artist: 'DVRST',
+      album: 'Phonk',
+      artwork:
+        'https://c.saavncdn.com/102/Motion-English-2021-20210925014435-500x500.jpg',
+      url: require('./assets/audio/Motion.mp3'),
+    },
+    {
+      id: 5,
+      title: 'Night Drive',
+      artist: 'Wilee',
+      album: 'Phonk',
+      artwork:
+        'https://c.saavncdn.com/333/Night-Drive-Unknown-2022-20221018061720-500x500.jpg',
+      url: require('./assets/audio/Night-Drive.mp3'),
+    },
+    {
+      id: 6,
+      title: 'REMXMBER',
+      artist: 'Roonin',
+      album: 'Phonk',
+      artwork:
+        'https://c.saavncdn.com/455/ASTRO-REMXMBER-English-2023-20230710175051-500x500.jpg',
+      url: require('./assets/audio/Remxmber.mp3'),
+    },
+    {
+      id: 7,
+      title: 'Tokyo Rain',
+      artist: 'Yabomag',
+      album: 'Phonk',
+      artwork:
+        'https://i1.sndcdn.com/artworks-9wgTryNpWjG8FgCC-l9KuMw-t500x500.jpg',
+      url: require('./assets/audio/Tokyo-Rain.mp3'),
+    },
+  ];
+  
